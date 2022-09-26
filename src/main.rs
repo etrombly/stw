38e4b3ad6f91e26386c12ddf8deb1fd9ccd6c5ff
Eric Trombly <etrombly@yahoo.com>
2022-09-26 12:17:08 -0500

    update signal handling

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,63 @@
-use anyhow::Result;
+#[cfg(any(target_os = "linux", target_os = "macos"))]
+use signal_hook::{consts::SIGINT, iterator::Signals, low_level};
 
-use askama::Template;
+use std::{io::Write, process::exit, thread};
 
-use std::net::TcpStream;
-use stw::{
-    config::load_config,
-    signal,
-    st::{
-        config::{ConfigTemplate, Folder},
-        deviceid::get_device_id,
-    },
-};
+use stw::{config::load_config, CHANNEL};
 
-fn main() -> Result<()> {
-    //let device_id = get_device_id("/home/eric/.config/syncthing/cert.pem")?;
-    //println!("{}", device_id);
+fn main() {
+    #[cfg(target_os = "windows")]
+    wintrap::trap(
+        &[
+            wintrap::Signal::CtrlC,
+            wintrap::Signal::CtrlBreak,
+            wintrap::Signal::CloseWindow,
+        ],
+        |_| {
+            unsafe {
+                if let Some(channel) = CHANNEL.get() {
+                    let mut channel = channel.lock().unwrap();
+                    let mut stream = channel.stream(0);
+                    let ctrl_c = format!("{}", 3 as char);
+                    while stream.write(ctrl_c.as_bytes()).is_err() {}
+                    while channel.send_eof().is_err() {}
+                    while channel.close().is_err() {}
+                }
+            }
+            exit(0);
+        },
+        move || {
+            let mut config = load_config(None).unwrap();
+            config.generate_config_templates().unwrap();
+        },
+    )
+    .unwrap();
 
-    signal::init();
-    let mut config = load_config(None)?;
-    config.generate_config_templates()?;
-
-    /*
-    let mut local_folders = Vec::new();
-    for folder in &config.folders {
-        local_folders.push(Folder {
-            id: folder.get_id(),
-            path: folder.local_path.clone(),
-        });
+    #[cfg(any(target_os = "linux", target_os = "macos"))]
+    {
+        init();
+        let mut config = load_config(None).unwrap();
+        config.generate_config_templates().unwrap();
     }
-    let local_config_template = ConfigTemplate {
-        path: config.get_folder(),
-        local_device_id: device_id.clone(),
-        local_device_name: "local".into(),
-        remote_device_id: device_id.clone(),
-        remote_device_name: "remote".into(),
-        folders: local_folders,
-    };
-    println!("{}", local_config_template.render().unwrap());
-    */
-    Ok(())
+}
+
+#[cfg(any(target_os = "linux", target_os = "macos"))]
+fn init() {
+    let mut signals = Signals::new(&[SIGINT]).unwrap();
+
+    thread::spawn(move || {
+        for sig in signals.forever() {
+            unsafe {
+                if let Some(channel) = CHANNEL.get() {
+                    let mut channel = channel.lock().unwrap();
+                    let mut stream = channel.stream(0);
+                    let ctrl_c = format!("{}", 3 as char);
+                    while stream.write(ctrl_c.as_bytes()).is_err() {}
+                    while channel.send_eof().is_err() {}
+                    while channel.close().is_err() {}
+                }
+                low_level::emulate_default_handler(SIGINT).unwrap();
+            }
+        }
+    });
 }
